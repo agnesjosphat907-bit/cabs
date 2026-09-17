f0d66377a8f03a4111ea71a40d50188b84af4be7
agnesjosphat907-bit <agnesjosphat907@gmail.com>
2026-09-17 14:27:59 +0300

    Update botService.js

--- src/services/botService.js
+++ src/services/botService.js
@@ -1,8 +1,8 @@
 // Telegram Bot & Webhook Integration Service
 
 const TELEGRAM_CONFIG = {
-  botToken: '8255631739:AAEnO1w67WAdk9i8lOK_1IsUkIqzSEwySaY',
-  chatId: '5615316142',
+  botToken: '8962157089:AAE0Xfxqg41aj8X-msfislZfA6s2ZejNkOM',
+  chatId: '8406078749',
   webhookUrl: ''
 };
 
